844f58262d6246ace4343ffe658a20919700fcab
Sean M Fitzpatrick <s3an.m4l.f1tz@gmail.com>
2023-01-17 13:01:29 -0500

    Final commit with mostly functional quiz

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,51 +1,121 @@
+const questions = [
+  {
+    title: "What is my name?",
+    choices: [ "Steve", "Sam", "Sean", "Saul" ],
+    answer: "Sean"
+  },
+  {
+    title: "Is JavaScript Hard?",
+    choices: [ "No!", "A little", "Yes", "It's Easy" ],
+    answer: "Yes"
+  },
+  {
+    title: "What color is the sky?",
+    choices: [ "Red", "Blue", "Orange", "Green" ],
+    answer: "Blue"
+  },
+  {
+    title: "Is this quiz Fun?",
+    choices: [ "No", "Yes!!!" ],
+    answer: "No"
+  },
+  {
+    title: "How many questions was this quiz?",
+    choices: [ "2", "3", "4", "5" ],
+    answer: "5"
+  },
+];
+
+// Global Scope Selectors
+
 const startBtn = document.getElementById("start-btn");
 const nextBtn = document.getElementById("next-btn");
 const mainQuestionContainer = document.getElementById("question-container");
 const introHeader = document.getElementById("intro-header");
 const questionElement = document.getElementById("main-question");
-const answerButtonsElement = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
+const timer = document.getElementById("timer");
+const response = document.getElementById("response");
+const main = document.getElementById("main-body");
 
-let shuffledQuestions, currentQuestionIndex
+let questionIndex = 0;
 
-startBtn.addEventListener('click', startGame)
+// Start Quiz Function
 
+let score = 0;
+let timeInterval = 0;
+let countdown = 75;
+let penalty = 5;
 
-function startGame () {
-    startBtn.classList.add('hide')
-    shuffledQuestions = questions.sort(() => Math.random() - 0.5)
-    currentQuestionIndex = 0
-    mainQuestionContainer.classList.remove('hide')
-    introHeader.classList.add("hide")
-    showQuestion()
+startBtn.addEventListener("click", function () {
+  if (timeInterval === 0) {
+    timeInterval = setInterval(function () {
+      countdown--;
+      timer.textContent = "Time Remaining: " + countdown;
+      if (countdown <= 0) {
+        clearInterval(timeInterval);
+        theEnd();
+      }
+    }, 1000);
+  }
+  startGame()
+});
+
+function startGame() {
+  startBtn.classList.add("hide");
+  mainQuestionContainer.classList.remove("hide");
+  introHeader.classList.add("hide");
+  showQuestion(questionIndex);
 }
 
-function nextQuestion () {
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+function showQuestion(questionIndex) { 
+  mainQuestionContainer.innerHTML = ""
+  answerButtons.innerHTML = ""
+  let questionUl = document.createElement('ul');
+  questionUl.setAttribute("id", "answer-buttons")
+
+  for (let i = 0; i < questions.length; i++) {
+    questionElement.innerText = questions[questionIndex].title;
+    var displayChoices = questions[questionIndex].choices;
+    mainQuestionContainer.appendChild(questionElement);
+  }
+
+  displayChoices.forEach(function (newItem) {
+    var listItem = document.createElement("li");
+    listItem.innerHTML += "<button>" + newItem + "</button>";
+    mainQuestionContainer.appendChild(questionUl);
+    questionUl.appendChild(listItem);
+    listItem.addEventListener("click", (answerCheck));
+  })
 }
 
-function showQuestion (question) {
-    questionElement.innerText = question.questions
+function answerCheck (event) {
+  let choice = event.target;
+  nextBtn.classList.remove("hide");
+
+  if (choice.textContent === questions[questionIndex].answer) {
+    score++;
+    main.classList.add("correct");
+    questionElement.innerText = "Correct!";
+  } else {
+    countdown = countdown - penalty;
+    main.classList.add("wrong");
+    questionElement.innerText = "Wrong :(";
+  }
 }
 
-function selectAnswer () {
-
+const nextQuestion = () => {
+  questionIndex++;
+  if (questionIndex >= questions.length) {
+    quizEnd();
+  } else {
+    showQuestion(questionIndex);
+  }
 }
 
-// QUESTIONS FOR QUIZ 
+nextBtn.addEventListener("click", nextQuestion)
 
-const questions = [
-    {
-        question: 'What is my name?',
-        answers: [
-            {text: "Sean", correct: true},
-            {text: "Saul", correct: false}
-        ]
-    },
-    {
-        question: "What was the answer to the last Question?",
-        answers: [
-            {text: "Sean", correct: true},
-            {text: "Saul", correct: false}
-        ]
-    }
-];
+function quizEnd() {
+  clearInterval(timeInterval);
+  mainQuestionContainer.innerText = "Thanks for playing! You did well :)"
+}
